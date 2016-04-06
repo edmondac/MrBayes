edmondac/MrBayes

commit 4ba14b89d02c677dca696b98e24b5df018d43f6e

Added new moves for fossils. Added Chi as major contributor. Added some AVX and FMA code and prepared for dynamic scaling.

src/mbbeagle.c
@@ -265,7 +265,7 @@ void LaunchBEAGLELogLikeForDivision(int chain, int d, ModelInfo* m, Tree* tree, 
             }
 
         if (beagleScalingFrequency != 0 && 
-            m->beagleComputeCount[chain] % (beagleScalingFrequency) == 0)
+            m->computeCount[chain] % (beagleScalingFrequency) == 0)
             {
             m->rescaleFreqOld = rescaleFreqNew = m->rescaleFreq[chain];
             for (i=0; i<tree->nIntNodes; i++)
@@ -349,7 +349,7 @@ void LaunchBEAGLELogLikeForDivision(int chain, int d, ModelInfo* m, Tree* tree, 
         }
     
     /* Count number of evaluations */
-    m->beagleComputeCount[chain]++;
+    m->computeCount[chain]++;
 }
 
 
@@ -432,7 +432,7 @@ void BeaglePrintResources()
         BeaglePrintFlags(beagleResources->list[i].supportFlags);
         MrBayesPrint ("\n\n");
         }
-    MrBayesPrint ("%s   BEAGLE vesion: %s\n", spacer, beagleGetVersion());
+    MrBayesPrint ("%s   BEAGLE version: %s\n", spacer, beagleGetVersion());
 }
 
 
@@ -815,7 +815,7 @@ int TreeCondLikes_Beagle (Tree *t, int division, int chain)
         if (p->upDateCl == YES)
             {
             /* remove old scalers */
-            if (m->scalersSet[chain][p->index] == YES && m->upDateAll == NO)
+            if (m->unscaledNodes[chain][p->index] == 0 && m->upDateAll == NO)
                 {
                 destinationScaleRead = m->nodeScalerIndex[chain][p->index];
                 cumulativeScaleIndex = m->siteScalerIndex[chain];
@@ -833,7 +833,6 @@ int TreeCondLikes_Beagle (Tree *t, int division, int chain)
             /* flip to the new workspace */
             FlipCondLikeSpace (m, chain, p->index);
             FlipNodeScalerSpace (m, chain, p->index);
-            m->scalersSet[chain][p->index] = NO;
             
             /* update conditional likelihoods */
             operations.destinationPartials    = m->condLikeIndex[chain][p->index       ];
@@ -842,7 +841,7 @@ int TreeCondLikes_Beagle (Tree *t, int division, int chain)
             operations.child2Partials         = m->condLikeIndex[chain][p->right->index];
             operations.child2TransitionMatrix = m->tiProbsIndex [chain][p->right->index];
 
-            /* All partials for tips are the same across omega catigoris, thus we are doing the following two if statments.*/
+            /* All partials for tips are the same across omega categories, thus we are doing the following two if statments.*/
             if (p->left->left== NULL && p->left->right== NULL)
                 chil1Step=0;
             else
@@ -853,9 +852,12 @@ int TreeCondLikes_Beagle (Tree *t, int division, int chain)
             else
                 chil2Step=1;
 
-            if (p->scalerNode == YES)
+            /* Now deal with scalers */
+            m->unscaledNodes[chain][p->index] = 1 + m->unscaledNodes[chain][p->left->index] + m->unscaledNodes[chain][p->right->index];
+
+            if (m->unscaledNodes[chain][p->index] >= m->rescaleFreq)
                 {
-                m->scalersSet[chain][p->index] = YES;
+                m->unscaledNodes[chain][p->index] = 0;
                 operations.destinationScaleWrite = m->nodeScalerIndex[chain][p->index];
                 cumulativeScaleIndex  = m->siteScalerIndex[chain];
                 }

src/mbbeagle.h
@@ -21,4 +21,6 @@ int    TreeCondLikes_Beagle (Tree *t, int division, int chain);
 int    TreeLikelihood_Beagle (Tree *t, int division, int chain, MrBFlt *lnL, int whichSitePats);
 int    TreeTiProbs_Beagle (Tree *t, int division, int chain);
 
+extern char *beagleGetVersion(void);
+
 #endif  /* __MBBEAGLE_H__ */

src/bayes.c
@@ -18,7 +18,8 @@
  *  With important contributions by
  *
  *  Paul van der Mark (paulvdm@sc.fsu.edu)
- *  Maxim Teslenko (maxim.teslenko@nrm.se)
+ *  Maxim Teslenko (maxkth@gmail.com)
+ *  Chi Zhang (zhangchicool@gmail.com)
  *
  *  and by many users (run 'acknowledgments' to see more info)
  *
@@ -956,8 +957,6 @@ int ReinitializeMrBayes (void)
     strcpy(sumtParams.sumtConType, "Halfcompat");    /* type of consensus tree output                 */
     sumtParams.calcTreeprobs = YES;                  /* should individual tree probs be calculated    */
     sumtParams.showSumtTrees = NO;                   /* should the individual tree probs be shown     */
-    sumtParams.printBrlensToFile = NO;               /* should brlens be printed to file              */
-    sumtParams.brlensFreqDisplay = 0.50;             /* threshold for printing brlens to file         */
     sumtParams.numTrees = 1;                         /* number of trees to summarize                  */
     sumtParams.numRuns = 2;                          /* number of analyses to summarize               */
     sumtParams.orderTaxa = YES;                      /* order taxa in trees ?                         */
@@ -968,6 +967,8 @@ int ReinitializeMrBayes (void)
     sumtParams.consensusFormat = FIGTREE;            /* format of consensus tree                      */
     strcpy (sumtParams.sumtOutfile, "temp");         /* output name for sumt command                  */
     sumtParams.HPD = YES;                            /* use Highest Posterior Density?                */
+    sumtParams.saveBrParams = NO;                    /* save branch/node params?                      */
+    sumtParams.minBrParamFreq = 0.50;                /* threshold for printing branch params to file  */
 
     /* sump parameters */
     strcpy(sumpParams.sumpFileName, "temp");         /* input name for sump command                   */

src/bayes.h
@@ -13,6 +13,7 @@
 #include <stdarg.h>
 #include <time.h>
 
+
 #ifdef USECONFIG_H
 #   include "config.h"
 #elif !defined (XCODE_VERSION) /* some defaults that would otherwise be guessed by configure */
@@ -21,6 +22,8 @@
 #   undef  HAVE_LIBREADLINE
 #   define UNIX_VERSION 1
 #   define SSE_ENABLED  1
+#   undef  AVX_ENABLED
+#   undef  FMA_ENABLED
 #   undef  MPI_ENABLED
 #   undef  BEAGLE_ENABLED
 #   undef  FAST_LOG
@@ -86,20 +89,54 @@ typedef double MrBFlt;      /* double used for parameter values and generally fo
                                if set to float MPI would not work becouse of use MPI_DOUBLE */
 typedef float CLFlt;        /* single-precision float used for cond likes (CLFlt) to increase speed and reduce memory requirement */
                             /* set CLFlt to double if you want increased precision */
-                            /* NOTE: CLFlt = double not compatible with SSE_ENABLED */
+                            /* NOTE: CLFlt = double not compatible with SSE_ENABLED, AVX_ENABLED or FMA_ENABLED */
 
-/* Define a compiler and vector size for the SSE code */
-#if defined (SSE_ENABLED)
-#  define FLOATS_PER_VEC 4
-#  if defined (WIN_VERSION)
-#    define MS_VCPP_SSE
-#    include <xmmintrin.h>
-#  else
-#    define GCC_SSE
-#    undef ICC_SSE
-#    include <xmmintrin.h>
+
+/*
+ * Make sure we define SIMD instruction flags in a stepwise manner. That is, if we have FMA, make sure we have AVX;
+ * if we have AVX, make sure we have SSE.
+ */
+#if defined (FMA_ENABLED)
+#  if !defined (AVX_ENABLED)
+#    define AVX_ENABLED
 #  endif
 #endif
+
+#if defined (AVX_ENABLED)
+#  if !defined (SSE_ENABLED)
+#    define SSE_ENABLED
+#  endif
+#endif
+
+/* Define compiler for appropriate SIMD vector data alignment and free operations */
+#if defined (SSE_ENABLED)
+#  if defined (WIN_VERSION)
+#    define MS_VCPP_SIMD
+#  else
+#    define GCC_SIMD
+#    undef ICC_SIMD
+#  endif
+#endif
+
+/* Include header for SSE code */
+#if defined (SSE_ENABLED)
+#  include <xmmintrin.h>
+#endif
+
+/* Include header for AVX code */
+#if defined (AVX_ENABLED)
+#  include <immintrin.h>
+#endif
+
+#if defined (FMA_ENABLED)
+#  include <immintrin.h>
+#endif
+
+/* Define vector code constants */
+#define VEC_NONE    0
+#define VEC_SSE     1
+#define VEC_AVX     2
+#define VEC_FMA     3
 
 /* For comparing floating points: two values are the same if the absolute difference is less then
    this value.
@@ -503,7 +540,6 @@ typedef struct node
     int             index;                  /*!< index to node (0 to numLocalTaxa for tips) */
     int             upDateCl;               /*!< cond likes need update?                    */
     int             upDateTi;               /*!< transition probs need update?              */
-    int             scalerNode;             /*!< is node scaling cond likes?                */
     int             isLocked;               /*!< is node locked?                            */
     int             lockID;                 /*!< id of lock                                 */
     int             isDated;                /*!< is node dated (calibrated)?                */
@@ -843,7 +879,7 @@ typedef struct
     } MoveType;
 
 /* max number of move types */
-#define NUM_MOVE_TYPES 100
+#define NUM_MOVE_TYPES 101
 
 /* struct holding info on each move */
 /* Note: This allows several different moves to affect the same parameter */
@@ -1243,17 +1279,21 @@ typedef struct modelinfo
     CLFlt       **clP;                      /* handy pointers to cond likes for ti cats     */
 #if defined (SSE_ENABLED)
     __m128      **clP_SSE;                  /* handy pointers to cond likes, SSE version    */
-    int         numSSEChars;                /* number of compact SSE character groups       */
-    CLFlt       *lnL_SSE;                   /* temp storage for log site likes              */
-    CLFlt       *lnLI_SSE;                  /* temp storage for log site invariable likes   */
+    int         numVecChars;                /* number of compact SIMD vectors               */
+    int         numFloatsPerVec;            /* number of floats per SIMD vector             */
+    CLFlt       *lnL_Vec;                   /* temp storage for log site likes              */
+    CLFlt       *lnLI_Vec;                  /* temp storage for log site invariable likes   */
+#if defined (AVX_ENABLED)
+    __m256      **clP_AVX;                  /* handy pointers to cond likes, AVX version    */
+#endif
 #endif
     MrBFlt      **cijks;                    /* space for cijks                              */
     int         **condLikeIndex;            /* index to cond like space for nodes & chains  */
     int         *condLikeScratchIndex;      /* index to scratch space for node cond likes   */
+    int         **unscaledNodes;            /* # unscaled nodes at a node                   */
+    int         *unscaledNodesScratch;      /* scratch # unscaled nodes at a node           */
     int         **nodeScalerIndex;          /* index to scaler space for nodes & chains     */
     int         *nodeScalerScratchIndex;    /* index to scratch space for node scalers      */
-    int         **scalersSet;               /* flags whether scalers are set                */
-    int         *scalersSetScratch;         /* scratch flag for whether scalers are set     */
     int         *siteScalerIndex;           /* index to site scaler space for chains        */
     int         siteScalerScratchIndex;     /* index to scratch space for site scalers      */
     int         **tiProbsIndex;             /* index to ti probs for branches & chains      */
@@ -1287,9 +1327,10 @@ typedef struct modelinfo
     int         printPosSel;                /* should selection be printed (YES/NO)         */
     int         printSiteOmegas;            /* should site omegas be printed (YES/NO)       */
 
-    /* likelihood calculator flags */
+    /* likelihood calculator flags and variables */
     int         useBeagle;                  /* use Beagle for this partition?               */
-    int         useSSE;                     /* use SSE for this partition?                  */
+    int         useVec;                     /* use SSE for this partition?                  */
+    int*        rescaleFreq;                /* rescale frequency for each chain             */
 
 #if defined (BEAGLE_ENABLED)
     /* Beagle variables */
@@ -1306,7 +1347,6 @@ typedef struct modelinfo
     int*        childTiProbIndices;         /* array of child ti prob indices (unrooted)    */
     int*        cumulativeScaleIndices;     /* array of cumulative scale indices            */
     int         rescaleBeagleAll;           /* set to rescale all nodes                     */
-    int*        rescaleFreq;                /* rescale frequency for each chain's tree      */
     int         rescaleFreqOld;             /* holds rescale frequency of current state     */
     int         recalculateScalers;         /* shoud we recalculate scalers for current state YES/NO */
     int*        succesCount;                /* count number of succesful computation since last reset of scalers */
@@ -1328,8 +1368,6 @@ typedef struct sumt
     char        sumtConType[100];      /* consensus tree type                           */
     int         calcTreeprobs;         /* should the individual tree probs be calculated*/
     int         showSumtTrees;         /* should the individual tree probs be shown     */
-    int         printBrlensToFile;     /* should branch lengths be printed to file      */
-    MrBFlt      brlensFreqDisplay;     /* threshold for printing branch lengths to file */
     int         numRuns;               /* number of independent analyses to summarize   */
     int         numTrees;              /* number of tree params to summarize            */
     int         orderTaxa;             /* order taxa in trees?                          */
@@ -1361,6 +1399,8 @@ typedef struct sumt
     int        *numFileTrees;          /* number of trees per file                      */
     int        *numFileTreesSampled;   /* number of trees sampled per file              */
     int         HPD;                   /* use highest posterior density?                */
+    int         saveBrParams;          /* save node/branch parameters?                  */
+    MrBFlt      minBrParamFreq;        /* threshold for printing branch params to file  */
     } Sumt;
 
 /* formats for consensus trees */

src/command.c
@@ -18,7 +18,8 @@
  *  With important contributions by
  *
  *  Paul van der Mark (paulvdm@sc.fsu.edu)
- *  Maxim Teslenko (maxim.teslenko@nrm.se)
+ *  Maxim Teslenko (maxkth@gmail.com)
+ *  Chi Zhang (zhangchicool@gmail.com)
  *
  *  and by many users (run 'acknowledgments' to see more info)
  *
@@ -934,14 +935,13 @@ int DoAbout (void)
     MrBayesPrint ("   program. Version 3 of MrBayes was a fully joint effort, started in the summer \n");
     MrBayesPrint ("   of 2002 when JPH visited Sweden on a grant from the Wenner-Gren Foundations.  \n");
     MrBayesPrint ("   Several others have contributed to the MrBayes code since then, most notably  \n");
-    MrBayesPrint ("   Paul van der Mark and Maxim Teslenko, both postdocs/programmers in Fredrik's  \n");
-    MrBayesPrint ("   lab. A large number of users and students, too many to list here, have also   \n");
-    MrBayesPrint ("   contributed importantly to the project (type 'Acknowledgments' for a list of  \n");
-    MrBayesPrint ("   some of them).                                                                \n");
+    MrBayesPrint ("   Paul van der Mark, Maxim Teslenko and Chi Zhang, all postdocs/programmers in  \n");
+    MrBayesPrint ("   Fredrik's lab. A large number of users and students, too many to list here,   \n");
+    MrBayesPrint ("   have also contributed importantly to the project (type 'Acknowledgments' for  \n");
+    MrBayesPrint ("   a list of some of them).                                                      \n");
     MrBayesPrint ("                                                                                 \n");
     MrBayesPrint ("   Since 2003, MrBayes has been distributed from SourceForge. Bugs can be repor- \n");
-    MrBayesPrint ("   ted to the MrBayes site on SourceForge or by contacting Maxim Teslenko        \n");
-    MrBayesPrint ("   (maxim.teslenko@nrm.se) directly.                                             \n");
+    MrBayesPrint ("   ted to the MrBayes site on SourceForge.                                       \n");
     MrBayesPrint ("   ---------------------------------------------------------------------------   \n");
 
     return (NO_ERROR);
@@ -965,8 +965,8 @@ int DoAcknowledgments (void)
     MrBayesPrint ("   instrumental in getting the Windows installer set up. Liu Liang and Dennis    \n");
     MrBayesPrint ("   Pearl helped integrate MrBayes with BEST.                                     \n");
     MrBayesPrint ("                                                                                 \n");
-    MrBayesPrint ("   Bug fixes and user support was provided by Paul van der Mark (2005-2007) and  \n");
-    MrBayesPrint ("   from 2010 by Maxim Teslenko (maxim.teslenko@nrm.se).                          \n");
+    MrBayesPrint ("   Bug fixes and user support was provided by Paul van der Mark (2005-2007),     \n");
+    MrBayesPrint ("   Maxim Teslenko (2010-2012) and Chi Zhang (2012-2015).                         \n");
     MrBayesPrint ("                                                                                 \n");
     MrBayesPrint ("   Our wives -- Edna Huelsenbeck and Eva Ronquist -- showed extraordinary        \n");
     MrBayesPrint ("   patience with us while we spent many late nights programming.                 \n");
@@ -11944,8 +11944,6 @@ int GetUserHelp (char *helpTkn)
         MrBayesPrint ("                   user starting tree. This allows you to have something         \n");
         MrBayesPrint ("                   between completely random and user-defined trees start        \n");
         MrBayesPrint ("                   the chain.                                                    \n");
-/*      MrBayesPrint ("   Savebrlens   -- This specifies whether branch length information is           \n");
-        MrBayesPrint ("                   saved on the trees.                                           \n"); */
         MrBayesPrint ("   Data         -- When Data is set to NO, the chain is run without data. This   \n");
         MrBayesPrint ("                   should be used only for examining induced priors. DO NOT SET  \n");
         MrBayesPrint ("                   'DATA' TO 'NO' UNLESS YOU KNOW WHAT YOU ARE DOING!            \n");
@@ -13115,6 +13113,12 @@ else if (!strcmp(helpTkn, "Set"))
         MrBayesPrint ("   Hpd           -- Determines whether credibility intervals will be given as the\n");
         MrBayesPrint ("                    region of Highest Posterior Density ('Yes') or as the inter- \n");
         MrBayesPrint ("                    val containing the median 95 %% of sampled values ('No').    \n");
+        MrBayesPrint ("   Savebrparams  -- Set this option to 'yes' to save all sampled branch and node \n");
+        MrBayesPrint ("                    parameter values to a separate file with the filename ending \n");
+        MrBayesPrint ("                    in '.brparams'. All partitions with a posterior probability  \n");
+        MrBayesPrint ("                    larger than Minbrparamfreq will be included.                 \n");
+        MrBayesPrint ("   Minbrparamfreq -- The minimum probability of partitions for which to save     \n");
+        MrBayesPrint ("                     parameter values to file if 'Savebrparams' is set to 'yes'. \n");
         MrBayesPrint ("                                                                                 \n");
         MrBayesPrint ("   Current settings:                                                             \n");
         MrBayesPrint ("                                                                                 \n");
@@ -13140,6 +13144,8 @@ else if (!strcmp(helpTkn, "Set"))
         MrBayesPrint ("   Calctreeprobs   Yes/No                   %s                                   \n", sumtParams.calcTreeprobs == YES ? "Yes" : "No");
         MrBayesPrint ("   Showtreeprobs   Yes/No                   %s                                   \n", sumtParams.showSumtTrees == YES ? "Yes" : "No");
         MrBayesPrint ("   Hpd             Yes/No                   %s                                   \n", sumtParams.HPD == YES ? "Yes" : "No");
+        MrBayesPrint ("   Savebrparams    Yes/No                   %s                                   \n", sumtParams.saveBrParams == YES ? "Yes" : "No");
+        MrBayesPrint ("   Minbrparamfreq  <number>                 %1.2lf                               \n", sumtParams.minBrParamFreq);
         MrBayesPrint ("                                                                                 \n");
         MrBayesPrint ("   ---------------------------------------------------------------------------   \n");
         }
@@ -14419,8 +14425,8 @@ void SetUpParms (void)
     PARAM (143, "Swapseed",       DoMcmcParm,        "\0");
     PARAM (144, "Runidseed",      DoMcmcParm,        "\0");
     PARAM (145, "Quitonerror",    DoSetParm,         "Yes|No|\0");
-    PARAM (146, "Printbrlens",    DoSumtParm,        "Yes|No|\0");
-    PARAM (147, "Brlensgeq",      DoSumtParm,        "\0");
+    PARAM (146, "Savebrparams",   DoSumtParm,        "Yes|No|\0");
+    PARAM (147, "Minbrparamfreq", DoSumtParm,        "\0");
     PARAM (148, "Minpartfreq",    DoMcmcParm,        "\0");
     PARAM (149, "Allchains",      DoMcmcParm,        "Yes|No|\0");
     PARAM (150, "Mcmcdiagn",      DoMcmcParm,        "Yes|No|\0");
@@ -14563,24 +14569,24 @@ void ShowNodes (TreeNode *p, int indent, int isThisTreeRooted)
         printf ("   ");
         if (p->left == NULL && p->right == NULL && p->anc != NULL)
             {
-            printf ("%*cN %d (l=%d r=%d a=%d) %1.15lf (%s) scalerNode=%d isDated=%d ",
-            indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc), p->length, p->label, p->scalerNode, p->isDated);
+            printf ("%*cN %d (l=%d r=%d a=%d) %1.15lf (%s) isDated=%d ",
+            indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc), p->length, p->label, p->isDated);
             }
         else if (p->left != NULL && p->right == NULL && p->anc == NULL)
             {
             if (isThisTreeRooted == NO)
                 {
                 if (p->label[0] == '\0' || p->label[0] == '\n' || p->label[0] == ' ')
-                    printf ("%*cN %d (l=%d r=%d a=%d) (---) scalerNode=%d ",
-                    indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc), p->scalerNode);
+                    printf ("%*cN %d (l=%d r=%d a=%d) (---) ",
+                    indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc));
                 else
-                    printf ("%*cN %d (l=%d r=%d a=%d) (%s) scalerNode=%d ",
-                    indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc), p->label, p->scalerNode);
+                    printf ("%*cN %d (l=%d r=%d a=%d) (%s) ",
+                    indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc), p->label);
                 }
             else
                 {
-                printf ("%*cN %d (l=%d r=%d a=%d) X.XXXXXX scalerNode=%d ",
-                indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc), p->scalerNode);
+                printf ("%*cN %d (l=%d r=%d a=%d) X.XXXXXX ",
+                indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc));
                 }
             }
         else
@@ -14588,11 +14594,11 @@ void ShowNodes (TreeNode *p, int indent, int isThisTreeRooted)
             if (p->anc != NULL)
                 {
                 if (p->anc->anc == NULL && isThisTreeRooted == YES)
-                    printf ("%*cN %d (l=%d r=%d a=%d) X.XXXXXX scalerNode=%d ",
-                    indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc), p->scalerNode);
+                    printf ("%*cN %d (l=%d r=%d a=%d) X.XXXXXX ",
+                    indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc));
                 else    
-                    printf ("%*cN %d (l=%d r=%d a=%d) %1.15lf scalerNode=%d ",
-                    indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc), p->length, p->scalerNode);
+                    printf ("%*cN %d (l=%d r=%d a=%d) %1.15lf ",
+                    indent, ' ', Dex(p), Dex(p->left), Dex(p->right), Dex(p->anc), p->length);
                 }
             }
         if (isThisTreeRooted == YES)

src/config.h.in
@@ -25,6 +25,12 @@
 /* Define to 1 if you want to compile SSE-enabled version */
 #undef SSE_ENABLED
 
+/* Define to 1 if you want to compile AVX-enabled version */
+#undef AVX_ENABLED
+
+/* Define to 1 if you want to compile FMA-enabled version */
+#undef FMA_ENABLED
+
 /* Define one of the UNIX, MAC or WINDOWS */
 #undef UNIX_VERSION
 #undef MAC_VERSION

src/configure.in
@@ -15,7 +15,7 @@ fi
 if test "x$enable_debug" == "xyes"; then
     : ${CFLAGS="-ggdb -Wall"}
 else
-    : ${CFLAGS="-O3"}; CFLAGS=$CFLAGS" -DNDEBUG";
+    : ${CFLAGS="-O3 -msse -mavx -mfma"}; CFLAGS=$CFLAGS" -DNDEBUG";
 fi
 AC_PROG_CC
 
@@ -68,6 +68,61 @@ if test "x$enable_sse" == "xyes"; then
     AC_DEFINE(SSE_ENABLED)
     , AC_MSG_RESULT(no)
     )
+fi
+
+AC_ARG_ENABLE(avx,
+[--enable-avx=[no/yes] enable avx instructions [default="yes"]]
+,,enable_avx="yes")
+if test "x$enable_avx" == "xyes"; then
+AC_MSG_CHECKING(for AVX support)
+AC_RUN_IFELSE(
+[AC_LANG_SOURCE(
+[
+#include <immintrin.h>
+#include <avxintrin.h>
+#include <x86intrin.h>
+int main(){
+int res;
+__m256          m1, m2;
+void *ptr = (void *) NULL;
+const size_t align = 32;
+res = posix_memalign(&ptr, align, align);
+m1 = _mm256_add_ps (m2, m1);
+return 0;
+}
+])],
+AC_MSG_RESULT(yes)
+AC_DEFINE(AVX_ENABLED)
+, AC_MSG_RESULT(no)
+)
+fi
+
+AC_ARG_ENABLE(fma,
+[--enable-fma=[no/yes] enable fma instructions [default="yes"]]
+,,enable_fma="yes")
+if test "x$enable_fma" == "xyes"; then
+AC_MSG_CHECKING(for FMA support)
+AC_RUN_IFELSE(
+[AC_LANG_SOURCE(
+[
+#include <immintrin.h>
+#include <avxintrin.h>
+#include <fmaintrin.h>
+#include <x86intrin.h>
+int main(){
+int res;
+__m256          m1, m2, m3;
+void *ptr = (void *) NULL;
+const size_t align = 32;
+res = posix_memalign(&ptr, align, align);
+m1 = _mm256_fmadd_ps (m2, m1, m3);
+return 0;
+}
+])],
+AC_MSG_RESULT(yes)
+AC_DEFINE(FMA_ENABLED)
+, AC_MSG_RESULT(no)
+)
 fi
 
 # ------------------------------------------------------------------------------

commit 0b3118dc7c4b3a9544c14c0caeb3bc732e68db3b

Proposed fixes to mbbeagle.[ch] to correct errors introduced by commit 4ba14b89d02c677dca696b98e24b5df018d43f6e on Dec 28, 2015.

src/mbbeagle.c
@@ -265,7 +265,7 @@ void LaunchBEAGLELogLikeForDivision(int chain, int d, ModelInfo* m, Tree* tree, 
             }
 
         if (beagleScalingFrequency != 0 && 
-            m->computeCount[chain] % (beagleScalingFrequency) == 0)
+            m->beagleComputeCount[chain] % (beagleScalingFrequency) == 0)
             {
             m->rescaleFreqOld = rescaleFreqNew = m->rescaleFreq[chain];
             for (i=0; i<tree->nIntNodes; i++)
@@ -349,7 +349,7 @@ void LaunchBEAGLELogLikeForDivision(int chain, int d, ModelInfo* m, Tree* tree, 
         }
     
     /* Count number of evaluations */
-    m->computeCount[chain]++;
+    m->beagleComputeCount[chain]++;
 }
 
 
@@ -855,7 +855,7 @@ int TreeCondLikes_Beagle (Tree *t, int division, int chain)
             /* Now deal with scalers */
             m->unscaledNodes[chain][p->index] = 1 + m->unscaledNodes[chain][p->left->index] + m->unscaledNodes[chain][p->right->index];
 
-            if (m->unscaledNodes[chain][p->index] >= m->rescaleFreq)
+            if (m->unscaledNodes[chain][p->index] >= m->rescaleFreq[chain])
                 {
                 m->unscaledNodes[chain][p->index] = 0;
                 operations.destinationScaleWrite = m->nodeScalerIndex[chain][p->index];
@@ -880,7 +880,7 @@ int TreeCondLikes_Beagle (Tree *t, int division, int chain)
                 operations.child1TransitionMatrix++;
                 operations.child2Partials+=chil2Step;
                 operations.child2TransitionMatrix++;
-                if (p->scalerNode == YES)
+                if (m->isScalerNode == YES)
                     {
                     operations.destinationScaleWrite++;
                     cumulativeScaleIndex++;

src/mbbeagle.h
@@ -21,6 +21,4 @@ int    TreeCondLikes_Beagle (Tree *t, int division, int chain);
 int    TreeLikelihood_Beagle (Tree *t, int division, int chain, MrBFlt *lnL, int whichSitePats);
 int    TreeTiProbs_Beagle (Tree *t, int division, int chain);
 
-extern char *beagleGetVersion(void);
-
 #endif  /* __MBBEAGLE_H__ */