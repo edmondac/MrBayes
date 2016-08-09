f67579bf9ca994235857726a596c39d68483ac41
Andreas (Kusalananda) Kähäri <andreas.kahari@icm.uu.se>
2016-08-09 15:12:53 +0200

    Updated to not parse "ls", etc.

--- test/batch_test/create_batch_test.bash
+++ test/batch_test/create_batch_test.bash
@@ -1,37 +1,42 @@
 #!/bin/bash
-j=0
-all_nex_files=$(ls $1*.nex)
-(
-echo "#NEXUS 
+
+dir="$1"
+outfile="batch_test.batch"
+
+cat >"$outfile" <<HEADER_END
+#NEXUS
 
 begin mrbayes;
 	set swapseed=1 seed=1 nowarn=yes autoclose=yes;
-"	
-for i in $all_nex_files
-do
 
-grep_res=$(grep -i "data;" $i)
-if [ "$grep_res" != "" ];
-then
+HEADER_END
 
-    let j++
-    echo "	[Data set # $j]
-	execute $i;"
-    grep_res=$(grep -i "lset" $i)
-    if [ "$grep_res" = "" ]; 
-    then
-	echo "	lset rates=invgamma nst=6;"
-    fi
+n=0
+for nex in "$dir"/*.nex; do
+    if grep -q -i "data;" "$nex"; then
+        n=$(( n + 1 ))
 
-    echo "	mcmc ng=1000 checkfr=100 file=crap;
+        cat <<DATASET_END
+	[Data set # $n]
+	execute $nex;
+DATASET_END
+
+        if ! grep -q -i "lset" "$nex"; then
+            cat <<LSET_END
+	lset rates=invgamma nst=6;
+LSET_END
+        fi
+
+        cat <<ANALYSIS_END
+	mcmc ng=1000 checkfr=100 file=crap;
 	mcmc ng=2000 append=yes file=crap;
 	sumt;
 	sump;
-"
-fi
-done
-echo "
+
+ANALYSIS_END
+    fi
+done >>"$outfile"
+
+cat >>"$outfile" <<FOOTER_END
 end;
-"
-) > batch_test.batch
-exit
+FOOTER_END
